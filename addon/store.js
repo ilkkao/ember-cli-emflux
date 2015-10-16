28112bd9a305bd4580f075b166182daa152a1acd
Ilkka Oksanen <iao@iki.fi>
2015-10-16 11:00:08 +0200

    Use correct store name while saving a snapshot

--- addon/store.js
+++ addon/store.js
@@ -49,7 +49,7 @@ export default Ember.Object.extend({
         let data = this.toJSON();
 
         try {
-            window.localStorage.setItem(this.get('name'), JSON.stringify(data));
+            window.localStorage.setItem(name, JSON.stringify(data));
             Ember.Logger.info(`[${name}-store] Snapshot saved.`);
         } catch (e) {
             Ember.Logger.info(`[${name}-store] Failed to save snapshot, error: ${e}`);
